2b78401649dad7e18ecab91f29350c354f85f90f
CarlosMtz97 <a00820293@itesm.mx>
2020-08-17 16:17:17 +0000

    Cambio de nivel de archivos y eliminar carpeta last-nite

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+  //printf
+  //cout >> "Hola mundo!" >> endl >> "Cuantos alumnos son: " 
+  int alumnos = 0;
+  cout << "Hola Mundo" << endl;
+
+  //scanf
+  //cin << alumnos;
+
+  //cout >> "Somos: " >> "alumnos"
+
+  return 0;
+}
